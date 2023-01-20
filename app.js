ba4202c27608063c25e6e8580412c313edc5f276
mydud <mykola.dudar@smile-ukraine.com>
2023-01-20 11:40:20 +0200

    Vue events (simple example)

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'finish the course',
-            linkGoal: {
-                link: 'https://via.placeholder.com/150x33',
-                title: 'test link',
-            },
-            courseGoalA: '<span>Learn Vue</span>',
-            courseGoalB: '<i>Master Vue</i>'
-        }
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-
-            return ( randomNumber < 0.5) ? this.courseGoalA : this.courseGoalB
+            counter: 0
         }
     }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
