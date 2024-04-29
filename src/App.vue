6a045fd4196fb5f99dd22cdd74cee3f02df3fc55
dudarmykola <dudarmykolastepan@gmail.com>
2024-04-29 16:38:51 +0300

    add ninja reaction timer app

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <button @click="toggleModalTwo">show modal two</button>
     </div>
   </div>
-  <div v-if="showModal">
+  <teleport v-if="showModal" to=".modals">
     <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up</a>
@@ -15,33 +15,49 @@
       <h1>Sign up for the giveaway</h1>
       <p>Grab your ninja pack for half price!</p>
     </Modal>
-  </div>
-  <div v-if="showModalTwo">
+  </teleport>
+  <teleport v-if="showModalTwo" to=".modals">
     <Modal @close="toggleModalTwo">
       <h1>Sign up yow the newsletter</h1>
     </Modal>
+  </teleport>
+  <div class="content">
+    <div class="actions">
+      <button @click="start">play</button>
+    </div>
+    <Block v-if="isPlaying" :delay="delay"/>
   </div>
+
 </template>
 
 <script>
 import Modal from '@/components/Modal.vue';
+import Block from '@/components/Block.vue';
 
 export default {
   name: 'App',
-  components: { Modal },
+  components: { Modal, Block },
   data() {
     return {
       title: 'My first vue app',
       showModal: false,
-      showModalTwo: false
+      showModalTwo: false,
+      isPlaying: false,
+      delay: null
+
     }
   },
   methods: {
     toggleModal() {
-      this.showModal = !this.showModal
+      this.showModal = !this.showModal;
     },
     toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo
+      this.showModalTwo = !this.showModalTwo;
+    },
+    start() {
+      this.delay = 2000 + Math.random() * 5000 ;
+      this.isPlaying = true;
+      console.log(this.delay);
     }
   }
 };
