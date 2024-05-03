df22d998165f0c5da7b2f376fb60a744b3f2b12c
dudarmykola <dudarmykolastepan@gmail.com>
2024-05-03 14:47:11 +0300

    Router added

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,36 @@
 <template>
-  <div class="content">
-    <h1>{{ title }}</h1>
-    <div class="actions">
-      <button @click="toggleModal">show modal</button>
-      <button @click="toggleModalTwo">show modal two</button>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
-  <teleport v-if="showModal" to=".modals">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Sign up for the giveaway</h1>
-      <p>Grab your ninja pack for half price!</p>
-    </Modal>
-  </teleport>
-  <teleport v-if="showModalTwo" to=".modals">
-    <Modal @close="toggleModalTwo">
-      <h1>Sign up yow the newsletter</h1>
-    </Modal>
-  </teleport>
-  <div class="content">
-    <div class="actions">
-      <button @click="start">play</button>
-    </div>
-    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-    <Results v-if="showResults" :score="score" />
-  </div>
-
+  <router-view />
 </template>
 
 <script>
-import Modal from '@/components/Modal.vue';
-import Block from '@/components/Block.vue';
-import Results from '@/components/Results.vue';
 
-export default {
-  name: 'App',
-  components: { Modal, Block, Results },
-  data() {
-    return {
-      title: 'My first vue app',
-      showModal: false,
-      showModalTwo: false,
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo;
-    },
-    start() {
-      this.delay = 2000 + Math.random() * 5000 ;
-      this.isPlaying = true;
-    },
-    endGame(reactTime) {
-      this.score = reactTime;
-      this.isPlaying = false;
-      this.showResults = true;
-    }
-  }
-};
 </script>
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
 }
 
-.actions {
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
   padding: 10px;
-  display: flex;
-  flex-direction: row;
-  width: 300px;
-  justify-content: space-around;
+  border-radius: 4px;
 }
 
-.actions button {
-  padding: 6px;
-  border-radius: 10px;
-  background: aliceblue;
-  border: 2px solid #ccc;
+#nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
 }
+
 </style>
